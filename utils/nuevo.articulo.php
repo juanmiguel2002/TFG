f2c781653d01f7846075284511b8ed61b492dbc4
juanmi <juanmi0802@gmail.com>
2023-05-17 14:45:38 +0200

    nuevo articulo

--- utils/nuevo.articulo.php
+++ utils/nuevo.articulo.php
@@ -3,12 +3,55 @@ define('ruta', 'http://localhost/CronistaGata/');
 
 require_once '../database/base_de_datos.php';
 require_once 'classes/articulo.php';
+require 'classes/File.php';
 
 $titulo = "";
 $temes = "";
 $exito = "";
 $error = "";
 $texto = "";
+$nombreImagen = '';
+
+    try {
+      if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        if ($nombreImagen != '') {
+          $tiposAceptados = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];//le pasamos los tipos aceptados que queremos subir
+          $imagen = new File('imagen', $tiposAceptados);//llamamos a la clase File y le pasamos los datos
+
+          $imagen->saveUploadFile('../upload/');// Asignamos la ruta donde se almacenará la img
+          $nombreImagen = $imagen->getFileName();//asignamos el nombre de la imagen
+          
+        }else{
+          $nombreImagen = "sin_imagen.jpg";
+        }
+        
+        $titulo = $_POST['titulo'];
+        $fk_temas = $_POST['temes'];
+        $texto = $_POST['texto'];
+        $fecha = date('Y-m-d');
+        
+        $sentencia = $pdo->prepare("INSERT INTO articulos (titulo, fecha, texto, imagen, fk_temas) VALUES (:titulo, :fecha, :texto, :imagen, :fk_temas)"); 
+        $sentencia->bindParam(':titulo', $titulo);
+        $sentencia->bindParam(':texto', $texto);
+        $sentencia->bindParam(':fecha', $fecha);
+        $sentencia->bindParam(':imagen', $nombreImagen);
+        $sentencia->bindParam(':fk_temas', $fk_temas);
+
+        //$paraneters = [':titulo' => $titulo, ':fecha' => $fecha, ':texto' => $texto, ':imagen' => $nombreImagen, ':id' => $fk_temas];
+        if ($sentencia->execute() === false) {
+            $error = "Error al insertar datos.";
+        } else {
+            $exito = "Datos ingresados correctamente.";
+            header('Location:'."panelControl.php");
+        }
+      }else {
+          $error = "Por favor ingresa todos los datos.";
+      }  
+        
+    } catch (FileException $fileException) {
+        $error = $fileException->getMessage();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -50,7 +93,7 @@ $texto = "";
               <?= $exito ?>
             </div>
           <?php endif ?>
-          <form action="" method="POST">
+          <form action="<?=$_SERVER['PHP_SELF']?>" method="POST" enctype="multipart/form-data">
             <div class="mb-3 row">
               <label for="Títol" class="col-sm-2 col-form-label">Títul</label>
               <div class="col-sm-10">
@@ -58,25 +101,31 @@ $texto = "";
               </div>
             </div>
             <div class="mb-3 row">
-              <label for="temas" class="col-sm-2 col-form-label">Temes</label>
+                <label for="temas" class="col-sm-2 col-form-label">Temas</label>
+                <div class="col-sm-10">
+                    <select class="form-control" name="temes" id="temes">
+                        <option value="">- Selecciona -</option>
+                        <?php
+                        $sentencia = $pdo->query("SELECT * FROM temas");
+                        $sentencia->execute();
+                        $temas = $sentencia->fetchAll(PDO::FETCH_CLASS, 'articulo');
+                        foreach ($temas as $tema) :
+                        ?>
+                            <option value="<?= $tema->getId() ?>" <?php if ($temes == $tema->getTema()) echo "selected" ?>><?= $tema->getTema() ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="imagen" class="col-sm-2 col-form-label">Imatge</label>
               <div class="col-sm-10">
-                <select class="form-control" name="temes" id="temes">
-                  <option value="">- Selecciona -</option>
-                  <?php $sentencia = $pdo->query("Select * FROM temas");
-                    $sentencia->execute();
-                    $articulos = $sentencia->fetchAll(PDO::FETCH_CLASS, 'articulo');
-                    foreach ( $articulos as $articulo) : ?>
-                        <option value="<?= $articulo->getTema() ?>" <?php if ($temes = $articulo->getTema()) echo "selected" ?>><?= $articulo->getTema() ?></option>
-                  <?php endforeach;?>
-                  
-                </select>
+                <input type="file" class="form-control" id="imagen" name="imagen" value="<?= $nombreImagen ?>">
               </div>
             </div>
-
             <div class="mb-3 row">
               <label for="direccion" class="col-sm-2 col-form-label">Descripción</label>
               <div class="col-sm-10">
-                <textarea class="form-control" id="texto" name="texto" rows="3" value="<?= $articulos[0]->getTexto() ?>"> <?= $articulos[0]->getTexto() ?> </textarea>
+                <textarea class="form-control" id="texto" name="texto" rows="3" value="<?= $texto ?>"> <?= $texto ?> </textarea>
               </div>
             </div>
 
@@ -119,11 +168,7 @@ $texto = "";
 
         const reader = new FileReader();
         reader.addEventListener('load', () => {
-          /*
-            Note: Now we need to register the blob in TinyMCEs image blob
-            registry. In the next release this part hopefully won't be
-            necessary, as we are looking to handle it internally.
-          */
+          
           const id = 'blobid' + (new Date()).getTime();
           const blobCache = tinymce.activeEditor.editorUpload.blobCache;
           const base64 = reader.result.split(',')[1];
---
+++ utils/panelControl.php
@@ -0,0 +1,123 @@
+<?php
+  define('ruta', 'http://localhost/CronistaGata/');
+  session_start();
+  if (((isset($_POST['usuario'])  && $_POST['usuario'] != "") && ($_POST['password'] != "" && isset($_POST['password'])))) {
+    $_SESSION['user'] = $_POST['usuario'];
+    $_SESSION['contra'] = $_POST['password'];
+  }
+
+  require '../database/base_de_datos.php';
+  require_once '../utils/classes/Paginacion.php'; 
+  require_once '../utils/classes/articulo.php';
+
+  $sentencia = $pdo->query("SELECT count(*) AS conteo FROM articulos");
+  $total_registros = $sentencia->fetchObject()->conteo;
+
+  $registros_por_pagina = 50; #Registros mostrar por página.
+  $pagina_actual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+
+  $paginas_a_mostrar = 15;
+  $paginacion = new Paginacion($total_registros, $registros_por_pagina, $pagina_actual, $paginas_a_mostrar);
+  $offset = $paginacion->getOffset(); # El offset es saltar X productos que viene dado por multiplicar la página - 1 * los productos por página
+  
+  $limit = $paginacion->getRegistrosPorPagina(); # El límite es el número de productos por página
+  $total_paginas = $paginacion->getTotalPaginas();
+  $pagina_actual = $paginacion->getPaginaActual();
+  $paginas_a_mostrar = $paginacion->getPaginasAMostrar();
+
+  $rango_inicio = max($pagina_actual - floor($paginas_a_mostrar / 2), 1);
+  $rango_fin = min($rango_inicio + $paginas_a_mostrar - 1, $total_paginas);
+  
+    # Ahora obtenemos los productos usando ya el OFFSET y el LIMIT
+  $sentencia = $pdo->prepare("SELECT DISTINCT id, titulo, fecha, tema FROM v_articulos order by 1 desc LIMIT ? OFFSET ?");
+  $sentencia->execute([$limit, $offset]);
+  $articulos = $sentencia->fetchAll(PDO::FETCH_CLASS,'articulo');
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <title>Cronista de Gata de Gorgos</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="<?= ruta ?>css/panelc.css" rel="stylesheet" type='text/css' />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz&display=swap" rel="stylesheet">
+  <link rel="shortcut icon" href="<?= ruta ?>img/periodic1.jpg" type="image/jpg" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">  
+</head>
+
+<body style="padding-bottom:2px;align-items:center">
+  <header>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="<?=ruta?>">CronistadeGata</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="panelControl.php">Home</a></li>
+          <li><a href="nuevo.articulo.php">Escriu nou</a></li>
+          <li><a href="temas.php">Temes</a></li>
+          <li><a href="../views/borradors.view.php">Borradors</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user dropdown"></span>Miguel</a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Conter</a></li>
+              <li><a href="#">Opcions</a></li>
+            </ul>
+          </li>
+          <li><a href="../utils/CerrarSession.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- BOTON HACIA ARRIBA -->
+    <a class="ir-arriba" javascript:void(0) title="Volver arriba">
+      <span class="fa-stack">
+        <i class="fa fa-circle fa-stack-2x"></i>
+        <i class="fa fa-arrow-up fa-stack-1x fa-inverse"></i>
+      </span>
+    </a>
+  </header>
+  <!-- CONTENEDOR DONDE SE MOSTRARAN TODOS LOS ARTICULOS -->
+  <div class="container">
+    <div class="form-inline my-2 my-lg-0" >
+      <input class="form-control mr-sm-2" type="text" name="campo" id="campo" placeholder="Buscar">
+      
+      <!-- <button class="btn btn-outline-success my-2 my-sm-0">Buscar</button> -->
+    </div>
+    <table class="table table-responsive" >
+      <thead>
+          <tr>
+              <th>Data</th>
+              <th>Articles</th>
+              <th>Opcions</th>
+          </tr>
+      </thead>
+      <tbody>
+        <?php 
+          foreach ($articulos as $articulo) : ?>
+          <tr>
+            <td><?=$articulo->getFecha()?></td>
+            <td><?=$articulo->getTitulo()?>
+              <br><strong><i>Publicat en <?= $articulo->getTema()?></i></strong>
+            </td>
+            <td>
+            <a href="editar.php?id=<?= $articulo->getId() ?>"><button type="button" class="btn btn-warning">Editar</button></a>
+            <a href="eliminar.php?id=<?= $articulo->getId() ?>" onclick="return confirm('Vols eliminar l`article?')"><button type="button" class="btn btn-danger">Eliminar</button></a>      
+            <a href="borrador.php?id=<?= $articulo->getId()?>" class="btn btn-secondary">Borrador</a>
+            </td>
+          </tr>
+          <?php endforeach;?>
+      </tbody>
+    </table>
+    <?php include "paginacion.view.php"; ?>
+  </div>
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
+  <script src="<?= ruta ?>js/main.js"></script>
+  <!-- <script src="<?= ruta ?>js/load.js"></script> -->
+</body>
+</html>
