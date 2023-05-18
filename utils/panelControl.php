488a3a340b61fbb76adddf6e521cb4fceab29de5
juanmi <juanmi0802@gmail.com>
2023-05-18 23:15:40 +0200

    panelControl

--- utils/panelControl.php
+++ utils/panelControl.php
@@ -127,9 +127,7 @@
     </table>
     <?php include "../views/paginacion.view.php"; ?>
   </div>
-  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
-  <script src="<?= ruta ?>js/main.js"></script>
 </body>
 </html>
