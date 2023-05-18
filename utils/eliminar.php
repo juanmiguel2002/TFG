8aaebe06025558862ed3cbb55390250b5b949649
juanmi <juanmi0802@gmail.com>
2023-05-18 22:09:50 +0200

    eliminar articulo

--- utils/eliminar.php
+++ utils/eliminar.php
@@ -1,25 +1,18 @@
 <?php 
     require_once '../database/base_de_datos.php';
-    $id = $_GET['id'];
-    $sentencia = $pdo->prepare("SELECT COUNT(*) as total FROM articulos");
+    $id = $_GET['id']; // seleccionamos el id del articulo
+    $sentencia = $pdo->prepare("SELECT COUNT(*) as total FROM articulos"); // hacemos un count para contar el maximo de articulos 
     $sentencia->execute();
     $resultado = $sentencia->fetch(PDO::FETCH_ASSOC);
     $total = $resultado['total'];
-    if (!is_numeric($id) || $id < 1 || $id <= $total) {
+    if (!is_numeric($id) || $id < 1 || $id < $total) { // comprobamos que no nos pasa ninguna letra o numero por debajo de 1 y el total
         header("HTTP/1.0 404 Not Found");
-        include("404.php");
+        include("404.php");//llamamos a la pagina de error 
         exit();
-    }else{
-
+    }else{ // si no se cumple borramos el articulo con el id pasado
         $sentencia = $pdo->prepare("DELETE from articulos where id = '$id'");
         $sentencia->execute();
-
-        // Crear la sentencia SQL para actualizar el valor del contador de identificación
-        $sql = "ALTER TABLE articulos AUTO_INCREMENT = 14157";
-
-        // Ejecutar la sentencia SQL
-        $pdo->exec($sql);
-        header('Location: panelControl.php');
-        exit();
+        
+        header('Location: panelControl.php'); 
     }  
 ?>
